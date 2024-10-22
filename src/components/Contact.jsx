d68399b933af585d52e40bd2fd88acda13f61177
danjys <danjys@gmail.com>
2024-10-21 20:54:52 -0400

    code clean up

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,23 @@
 import { motion } from 'framer-motion';
 import WaterDropGrid from './WaterDrop';
 import CustomName from './BubbleText';
+import './contact.css';
 
 const Contact = () => (
-    <motion.section
-      id="contact"
-      className="min-h-screen flex items-center justify-center bg-gray-800 text-white"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
-      <div>
-      <h2 className="text-3xl font-bold">Get in Touch</h2>
+  <motion.section
+    id="contact"
+    className="min-h-screen flex items-center justify-center bg-gray-900 text-white"
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 1 }}
+  >
+    <div className='paddingDiv'>
       <CustomName></CustomName>
-        <WaterDropGrid></WaterDropGrid>
-      </div>
-      
-  
-    </motion.section>
-  );
+      <WaterDropGrid></WaterDropGrid>
+    </div>
 
-  export default Contact;
+
+  </motion.section>
+);
+
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,39 @@
 import { motion } from 'framer-motion';
+import githubicon from "../assets/githublink.png";
+import PORTFOLIO from "../assets/portfolio.png";
 
 const projects = [
     {
       name: 'JARVIS-Style AI Assistant with Advanced Voice Control and Facial Recognition',
       description: 'Full stack development : Engineered a JARVIS-like AI assistant using Python, HTML/JavaScript, and Eel.js to create a seamless connection between a powerful backend and an interactive frontend. This AI system mimics Tony Stark\'s iconic assistant, offering intelligent automation, real-time responses, and advanced security protocols ',
       link: 'https://github.com/danjys/jarvis',
+      thumbnail: PORTFOLIO
     },
     {
       name: 'Airbnb',
       description: 'Vacation rental platform',
       link: 'https://github.com/danjys/Robinhood',
+      thumbnail: PORTFOLIO
     },
     {
       name: 'Robinhood',
       description: 'A trading platform and a news scraper. Stock analysis using AI.',
       link: 'https://github.com/danjys/Robinhood',
+      thumbnail: PORTFOLIO
     },
     {
       name: 'Coinbase',
       description: 'Blockchain app.',
       link: 'https://github.com/danjys/Robinhood',
+      thumbnail: PORTFOLIO
     }
     // Add more projects here
   ];
   
   const Projects = () => {
     return (
-      <section id="projects" className="min-h-screen bg-gradient-to-r from-purple-400 to-blue-500 bg-gray-100 py-20">
+      <section id="projects" className="min-h-screen bg-gradient-to-r bg-gray-900 py-20">
         <div className="container mx-auto px-8">
-          <h2 className="text-4xl font-bold text-center mb-12 text-gray-800">
-            Projects
-          </h2>
-          
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {projects.map((project, index) => (
               <motion.div
@@ -42,21 +44,21 @@ const projects = [
               transition={{ duration: 0.5, delay: index * 0.2 }}
             >
               <img
-                src="https://via.placeholder.com/150"
+                src={project.thumbnail}
                 alt={project.name}
-                className="w-full h-40 object-cover mb-4"
+                className="w-full  mb-4"
               />
-              <h3 className="text-2xl font-bold text-gray-800 mb-4">
+              <h3 className="text-small font-bold text-gray-800 mb-4">
                 {project.name}
               </h3>
-              <p className="text-gray-600 mb-6">{project.description}</p>
+              <p className="text-gray-600 text-xs mb-6">{project.description}</p>
               <a
                 href={project.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 hover:text-blue-700 transition-colors"
               >
-                View Project
+                <img src={githubicon} alt="View Project" />
               </a>
             </motion.div>
             
